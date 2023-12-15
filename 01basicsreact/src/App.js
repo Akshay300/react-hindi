299d3eab78fef61dd6253fd99680d185dd5be89a
akshay <akshayap4141@gmail.com>
2023-12-15 19:58:09 +0530

    creating react project

---
+++ 01basicsreact/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function MyButton() {
+  return (
+    <>
+      <button>Click Me</button>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Akshay Ashok Patil, is going to learn React js with Hands on</h1>
+      <MyButton/>
+    </div>
+  );
+}
+
+export default App;
